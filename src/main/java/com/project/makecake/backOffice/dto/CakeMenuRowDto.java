f6c3b165187388b82aa85f0ec939e9a90ab54864
minjoo-kou <kouminjoo0510@gmail.com>
2022-03-14 16:34:43 +0900

    구민주 - 케이크 메뉴 & 옵션 등록 백오피스 수정

--- src/main/java/com/project/makecake/backOffice/dto/CakeMenuRowDto.java
+++ src/main/java/com/project/makecake/backOffice/dto/CakeMenuRowDto.java
@@ -1,5 +1,6 @@
 package com.project.makecake.backOffice.dto;
 
+import com.project.makecake.model.CakeMenuPriceState;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,12 +11,14 @@ import lombok.Setter;
 public class CakeMenuRowDto {
     private String type;
     private String size;
+    private String priceState;
     private String price;
     private String moreInfo;
 
-    public CakeMenuRowDto(String type, String size, String price, String moreInfo){
+    public CakeMenuRowDto(String type, String size, String priceState, String price, String moreInfo){
         this.type = type;
         this.size = size;
+        this.priceState = priceState;
         this.price = price;
         this.moreInfo = moreInfo;
     }
