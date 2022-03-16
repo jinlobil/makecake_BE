d76e7449309aad0f0447c31c5e8333627eba313a
hyeji <laon1551@naver.com>
2022-03-16 10:10:05 +0900

    고혜지-케이크 사진 모달에 storeId 추가

--- src/main/java/com/project/makecake/responseDto/CakeResponseDto.java
+++ src/main/java/com/project/makecake/responseDto/CakeResponseDto.java
@@ -11,6 +11,7 @@ public class CakeResponseDto {
 
     private Long cakeId;
     private String img;
+    private Long storeId;
     private String storeName;
     private int likeCnt;
     private boolean myLike;
@@ -19,6 +20,7 @@ public class CakeResponseDto {
     public CakeResponseDto(Cake cake,boolean myLike) {
         this.cakeId = cake.getCakeId();
         this.img = cake.getUrl();
+        this.storeId = cake.getStore().getStoreId();
         this.storeName = cake.getStore().getName();
         this.likeCnt = cake.getLikeCnt();
         this.myLike = myLike;
