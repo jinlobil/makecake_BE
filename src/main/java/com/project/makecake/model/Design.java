4b7c65c06b13947e5e317a59583184c7f755dfbd
hyeji <laon1551@naver.com>
2022-03-29 20:43:15 +0900

    Fix: 아이디 생성 전략 몇개 롤백
    이미 다른 테이블과 연결되어 있는 테이블들 롤백

--- src/main/java/com/project/makecake/model/Design.java
+++ src/main/java/com/project/makecake/model/Design.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Entity
 public class Design extends Timestamped{
 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long designId;
 
--- src/main/java/com/project/makecake/model/Review.java
+++ src/main/java/com/project/makecake/model/Review.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Review extends Timestamped{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long reviewId;
 
     @Column(nullable = false)
