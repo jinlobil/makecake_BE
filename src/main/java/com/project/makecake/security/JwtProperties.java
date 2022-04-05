8ad173f0f78a854752dc98ff0b5e62736fb3e54c
devssk <97424104+devssk@users.noreply.github.com>
2022-04-05 21:42:41 +0900

    Update JwtProperties.java

--- src/main/java/com/project/makecake/security/JwtProperties.java
+++ src/main/java/com/project/makecake/security/JwtProperties.java
@@ -4,7 +4,7 @@ public interface JwtProperties {
     // 시크릿 키
     String secretKey = "ourmakecakegoodluck";
     // 토큰 유효 시간 (60000밀리초 * 60분 * 24시간 * 3 = 3일)
-    Long tokenValidTime = 60000*60*24*3L;
+    Long tokenValidTime = 10000L;
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
