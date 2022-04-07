edfdbfa1494aafe66545b0d0ba4acb72ee700dc6
devssk <sksong419@naver.com>
2022-04-08 00:19:18 +0900

    Feat: 추가 시큐리티 권한 설정  추가 백오피스 기능 시큐리티 권한 설정 추가

--- src/main/java/com/project/makecake/controller/backoffice/BOCakeController.java
+++ src/main/java/com/project/makecake/controller/backoffice/BOCakeController.java
@@ -5,6 +5,7 @@ import com.project.makecake.model.Cake;
 import com.project.makecake.model.Store;
 import com.project.makecake.service.backoffice.BOCakeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,18 +20,21 @@ public class BOCakeController {
     private final BOCakeService BOCakeService;
 
     // (관리자용) 가게별 케이크 사진 리스트 조회 API
+    @Secured("ROLE_ADMIN")
     @GetMapping("/back-office/stores/{storeId}/cakes")
     public List<Cake> GetCakeListAtBackoffice(@PathVariable long storeId) {
         return BOCakeService.GetCakeListAtBackoffice(storeId);
     }
 
     // (관리자용) 케이크 사진 삭제 API
+    @Secured("ROLE_ADMIN")
     @DeleteMapping("/back-office/cakes/{cakeId}")
     public long deleteCake(@PathVariable long cakeId) {
         return BOCakeService.deleteCake(cakeId);
     }
 
     // (관리자용) 케이크 사진 저장 API
+    @Secured("ROLE_ADMIN")
     @PostMapping("/back-office/stores/{storeId}/cakes")
     public void addCakeList(
             @PathVariable long storeId,
